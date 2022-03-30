6c647494076f0b0a95590e2243990dd6753af1fe
parimal-kini <parimalkini@gmail.com>
2022-03-30 20:09:53 +0530

    2

---
+++ 2_ConditionalsLoops/src/com/parimalkini/DoWhile.java
@@ -0,0 +1,11 @@
+package com.parimalkini;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 2, j=1;
+        do {
+            System.out.println(i+" * "+j+" = "+i*j);
+            j++;
+        }while (j<=10);
+    }
+}
