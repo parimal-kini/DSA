1fa4ab994281f47a62c94969ad1394a6024f0f36
parimal-kini <parimalkini@gmail.com>
2022-04-01 21:38:34 +0530

    4

--- 4_Functions/src/com/parimalkini/VarArgs1_10.java
+++ 4_Functions/src/com/parimalkini/VarArgs1_10.java
@@ -5,7 +5,7 @@ public class VarArgs1_10 {
 //        Overloaded functions called via different parameters
         display("Parimal", "Vaishnavi");
         display(1,2,3);
-        display(69,"Parimal","Vaishnavi");
+        display(69,"Vaishnavi","Parimal","Kini");
     }
     static void display(String... name){
 //        For Loop to print names in var args
@@ -19,7 +19,7 @@ public class VarArgs1_10 {
     }
 //     Funtion oveerloading (different parameters)
     static void display(int n, String... name){
-            System.out.println(n);
+            System.out.print(n+" ");
         for( String s:name)
             System.out.print(s+" ");
     }
