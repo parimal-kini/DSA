2fac00c4b238bba38280f5b58701b70a382f0911
parimal-kini <parimalkini@gmail.com>
2022-04-02 23:27:00 +0530

    4

--- 4_Functions/src/com/parimalkini/Sum_Of_First_N_Natural_Nos_8.java
+++ 4_Functions/src/com/parimalkini/Sum_Of_First_N_Natural_Nos_8.java
@@ -7,7 +7,6 @@ public class Sum_Of_First_N_Natural_Nos_8 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number : ");
         int n = scanner.nextInt();
-
         System.out.println("Sum of all numbers upto "+n+"is : "+sum(n));
     }
     static int sum(int n){
